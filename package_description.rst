d9df63b61b1a3fb2c6688f634bf4bdfcf58d9159
lanselambor <757016515@qq.com>
2018-06-28 06:53:07 +0000

    update for pypi

---
+++ package_description.rst
@@ -0,0 +1,2 @@
+This python module is for Seeed LTE Pi Hat which uses ublox lara r2 4G cellular module.
+Runs on raspberry pi only.
--- setup.py
+++ setup.py
@@ -5,15 +5,17 @@ try:
 except ImportError:
 	from distutils.core import setup
 
+with open('package_description.rst', 'r') as f:
+	long_description = f.read()
+
 
 setup(
       name='ublox-lara-r2',
-      version='0.0.1',
+      version='0.0.5',
       packages = find_packages(),
-      description='LTE Pi driver Hat using celluar module ublox lara r2 LTE Cat.1.',
+      description='LTE Pi Hat driver using celluar module ublox lara r2 LTE Cat.1.',
       long_description=long_description,
-      long_description_content_type="text/markdown",
-      author='Lambor Fang',
+      author='Lambor',
       author_email='lanselambor@gmail.com',
       url='https://www.github.com/Seeed-Studio/ublox_lara_r2_pi_hat',
       license='MIT',
@@ -24,4 +26,5 @@ setup(
           "License :: OSI Approved :: MIT License",
           "Operating System :: POSIX :: Linux",
       ),
+      keywords=['ublox lara r2', 'ublox', 'R211', 'R204', 'LTE', 'Cat.1'],
      )
