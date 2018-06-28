5b1addd0bfca122f7d9fddc11efc70d9a0e8406a
lanselambor <757016515@qq.com>
2018-06-28 03:13:55 +0000

    modified setup.py

--- setup.py
+++ setup.py
@@ -6,13 +6,22 @@ except ImportError:
 	from distutils.core import setup
 
 
-setup(name='ublox-lara-r2',
-      version='1.0.0',
+setup(
+      name='ublox-lara-r2',
+      version='0.0.1',
       packages = find_packages(),
-      description='LTE Pi Hat using celluar module ublox lara r2 LTE Cat.1 driver',
-      author='Lmabor Fang',
+      description='LTE Pi driver Hat using celluar module ublox lara r2 LTE Cat.1.',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      author='Lambor Fang',
       author_email='lanselambor@gmail.com',
       url='https://www.github.com/Seeed-Studio/ublox_lara_r2_pi_hat',
       license='MIT',
       scripts=["bin/rpirtscts"],
+      classifiers=(
+          "Programming Language :: Python :: 2",
+          "Programming Language :: Python :: 2.7",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: POSIX :: Linux",
+      ),
      )
